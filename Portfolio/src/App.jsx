e806ecb52fd75b84aa7dad0a83f4b12d0c2dd05c
adilrana03 <adilrana2014.com>
2023-11-03 12:19:19 +0530

    added some sections

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,19 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-
+import { ClickToComponent } from 'click-to-react-component'
 const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <ClickToComponent />
 
 
           <Navbar />
           <Hero />
         </div>
         <About />
-        {/* <Experience /> */}
+        <Experience />
+        <Feedbacks />
         <Tech />
         <Works />
         <div className='relative z-0'>
